278fa066a8b718057f4989b955fb1c90b2aa4b38
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-25 22:47:33 +0000

    Automatically backed up by Learn

--- lib/MucsicLibraryController.rb
+++ lib/MucsicLibraryController.rb
@@ -70,6 +70,7 @@ def play_song
   input = gets.strip.to_i
   song = Song.all[input]
   
-  puts "Playing #{song.name} by #{song.artist.name}" if song 
+  puts "Playing #{song.name} by #{song.artist.name}" if song
+end 
 
 end 
